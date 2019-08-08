38ac0274994917fdb23ac1b1f6eafd778915e60c
anthonyjlatorre <anthonyjlatorre@gmail.com>
2019-08-08 11:29:06 -0500

    First Commit

---
+++ testingSite/src/main/java/com/ajl/TestingSiteApplication.java
@@ -0,0 +1,15 @@
+package com.ajl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSiteApplication.class, args);
+		System.out.println("Starting");
+		
+	}
+
+}
